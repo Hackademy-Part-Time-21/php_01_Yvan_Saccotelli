5ca44215b9b0f741a5ec966280141bca1854eefb
Yvan <yvan@192.168.1.8>
2024-01-24 17:33:48 +0100

    sistemato array

--- variabili.php
+++ variabili.php
@@ -28,21 +28,16 @@
 // var_dump("$stringa");
 
 
-$words1 = ['una','vita', 'colle', 'mi', 'rosso', 
-            [ 'oscura','era',
-                [ 'mezzo',
-                ['cammin', 'Nel', 
-                [ 'selva', 'la', 
-                [ 'via', 'una',  ] 
-            ],
-            ] 
-            ],
-            'ritrovai', 'per' ], 'diritta' 
-            ]; 
 
-$words2 = [ 'elemento2' => 'nostra', 
-                'elemento3' => [ 'Virgilio', 'smarrita', 'ché' ] 
-            ];
+
+$words1 = ['una', 67, 'vita', 'colle', 'mi', 'rosso', 
+                ['oscura', 'era', 89, 
+                    ['mezzo', ['cammin', 'Nel', ['selva', 'la', ['via', 'una', true]]],],
+                'ritrovai', 'per'], 
+            'diritta'];
+
+$words2 = ['elemento1' => 25.89, 'elemento2' => 'nostra', 'elemento3' => ['Virgilio', 'smarrita', 'ché']];
 $words1 [] = "del";
-$result = $words1[5][2][1][1] ." " . $words1[5][2][0] . " " .$words1[7]. " " . $words1[5][2][1][0];
+$words1 [] = "in";
+$result = $words1[6][3][1][1] ." " . $words1[6][3][0] . " " .$words1[8]. " " .$words1[6][3][1][0]." " .$words1[4]. " " .$words1[6][4]. " ".$words1[9]. " ".$words1[0]. " ";
 var_dump($result);
